09300bd7b836f58781e95de70e92fa2a696343ec
maria <ma.usachova@gmail.com>
2021-12-08 12:26:44 +0300

    классы

---
+++ my_queue.py
@@ -0,0 +1,127 @@
+class MyQueue:
+    def __init__(self, subjects, subj_dict, namesofsubjs, slovechki):
+        self.subjects = subjects
+        self.subj_dict = subj_dict
+        self.namesofsubjs = namesofsubjs
+        self.slovechki = slovechki
+
+    def read_list_from_file(self, filename):
+        list1, list2 = [], []
+        f = open(filename + '.txt', 'r')
+        is_first_part = True
+        for line in f:
+            if line == "...\n":
+                is_first_part = False
+                continue
+            if is_first_part:
+                list1.append(line)
+            else:
+                list2.append(line)
+        f.close()
+        return list1, list2
+
+    def update_file(self, filename, list1, list2):
+        f = open(filename + '.txt', 'w')
+        for line in list1:
+            f.write(line)
+        f.write("...\n")
+        for line in list2:
+            f.write(line)
+        f.close()
+
+    def write_queue(self, list1, list2, s):
+        for line in enumerate(list1):
+            s += str(line[0] + 1) + '. ' + line[1]
+        if list2:
+            s += '...\n'
+            for line in enumerate(list2):
+                s += 'n-' + str(len(list2) - line[0] - 1) + '. ' + line[1]
+        return s
+
+    def check_words(self, msg):
+        words = ['!очистить', '!запись', '!удалиться', '!очередь ', '!дима', '!тейлор', '!команды']
+        tests_commands = ['!!очиститьвсё', '!!смотретьвсё']
+        words += tests_commands
+        words += self.slovechki.keys()
+        for word in words:
+            if word in msg:
+                return True
+        return False
+
+    def record(self, text, name, mes):
+        to_first_list = True
+        l = text.split()[1:]
+        if l and l[0] == 'последним':
+            l = l[1:]
+            to_first_list = False
+
+        if l and l[0] in self.subj_dict:
+            number = self.subj_dict[l[0]]
+
+            listFirstPart, listSecondPart = self.read_list_from_file(self.subjects[number])
+
+            if listFirstPart.count(name + '\n') or listSecondPart.count(name + '\n'):
+                mes = 'вы уже записаны в очередь на ' + self.namesofsubjs[number] + ' !'
+            else:
+                if to_first_list:
+                    listFirstPart.append(name + '\n')
+                    mes = 'вы добавлены в очередь на ' + self.namesofsubjs[number]
+                else:
+                    listSecondPart.reverse()
+                    listSecondPart.append(name + '\n')
+                    listSecondPart.reverse()
+                    mes = 'вы добавлены в очередь на ' + self.namesofsubjs[number] + ' последним'
+
+            self.update_file(self.subjects[number], listFirstPart, listSecondPart)
+        return mes
+
+    def show_queue(self, text, mes):
+        l = text.split()[1:]
+        if l and l[0] in self.subj_dict:
+            number = self.subj_dict[l[0]]
+            listFirstPart, listSecondPart = self.read_list_from_file(self.subjects[number])
+            mes = 'пожилая очередь на ' + self.namesofsubjs[number] + ':\n'
+            mes = self.write_queue(listFirstPart, listSecondPart, mes)
+        return mes
+
+    def clear_queue(self, text, mes):
+        l = text.split()[1:]
+        if l and l[0] in self.subj_dict:
+            number = self.subj_dict[l[0]]
+            listFirstPart, listSecondPart = self.read_list_from_file(self.subjects[number])
+            listFirstPart.clear()
+            listSecondPart.clear()
+            mes = 'очередь на ' + self.namesofsubjs[number] + ' очищена'
+            self.update_file(self.subjects[number], listFirstPart, listSecondPart)
+        return mes
+
+    def remove_from_queue(self, text, name, mes):
+        l = text.split()[1:]
+        if l and l[0] in self.subj_dict:
+            number = self.subj_dict[l[0]]
+            listFirstPart, listSecondPart = self.read_list_from_file(self.subjects[number])
+            if listFirstPart.count(name + '\n'):
+                listFirstPart.remove(name + '\n')
+            if listSecondPart.count(name + '\n'):
+                listSecondPart.remove(name + '\n')
+            mes = 'вы удалены из очереди на ' + self.namesofsubjs[number]
+            self.update_file(self.subjects[number], listFirstPart, listSecondPart)
+        return mes
+
+    def remove_all(self):
+        mes = ''
+        for subj in self.subjects:
+            listFirstPart, listSecondPart = self.read_list_from_file(subj)
+            listFirstPart.clear()
+            listSecondPart.clear()
+            mes += 'очередь на ' + subj + ' очищена\n'
+            self.update_file(subj, listFirstPart, listSecondPart)
+        return mes
+
+    def show_all(self):
+        mes = ''
+        for subj in self.subjects:
+            listFirstPart, listSecondPart = self.read_list_from_file(subj)
+            mes += 'пожилая очередь на ' + subj + ':\n'
+            mes = self.write_queue(listFirstPart, listSecondPart, mes)
+        return mes
---
+++ server.py
@@ -0,0 +1,125 @@
+import vk_api
+from vk_api.utils import get_random_id
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+import random
+import os
+from datetime import datetime, date
+
+from my_queue import MyQueue
+
+
+class Server:
+    def __init__(self, token, group_id):
+        self.vk_session = vk_api.VkApi(token=token)
+        self.longpoll = VkBotLongPoll(self.vk_session, group_id)
+        self.vk = self.vk_session.get_api()
+
+    def get_name(self, event):
+        user_get = self.vk.users.get(user_ids=str(event.object.from_id))
+        full_name = user_get[0]['first_name'] + " " + user_get[0]['last_name']
+        return full_name
+
+    def start(self, subjects, subj_dict, namesofsubjs, slovechki):
+        queue = MyQueue(subjects, subj_dict, namesofsubjs, slovechki)
+        for event in self.longpoll.listen():
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                if event.from_chat:
+                    text = event.object['text'].lower()
+                    if queue.check_words(text):
+                        mes = 'чево'
+                        name = self.get_name(event)
+
+                        if '!запись' in text:
+                            mes = queue.record(text, name, mes)
+
+                        if '!очередь' in text:
+                            mes = queue.show_queue(text, mes)
+
+                        if '!очистить' in text:
+                            mes = queue.clear_queue(text, mes)
+
+                        if '!удалиться' in text:
+                            mes = queue.remove_from_queue(text, name, mes)
+
+                        if '!дима' in text:
+                            l = text.split()[1:]
+                            if l:
+                                if l[0] == 'не':
+                                    f = open('prikol/dima.txt', 'r')
+                                    last_date_str = f.readline()
+                                    last_date = [int(x) for x in last_date_str.rstrip().split('-')]
+                                    record = int(f.readline().rstrip().split(" ")[1])
+                                    cnt = datetime.today().date() - date(last_date[0], last_date[1], last_date[2])
+                                    mes = 'опять?..😢 \n дней без димы в вузе: ' + str(cnt.days).split()[0] + '..😵'
+                                    f.close()
+                                    if int(cnt.days) > record:
+                                        f = open('prikol/dima.txt', 'w')
+                                        f.write(last_date_str + 'рекорд: ' + str(cnt.days))
+                                        f.close()
+                                elif l[0] == 'пришёл':
+                                    f = open('prikol/dima.txt', 'r')
+                                    last_date = f.readline().rstrip()
+                                    record = f.readline()
+                                    f.close()
+                                    f = open('prikol/dima.txt', 'w')
+                                    f.write(str(datetime.today().date()) + '\n' + record)
+                                    mes = 'во дела😳🤯 \n дней без димы в вузе: 0'
+                                    f.close()
+                                elif l[0] == 'рекорд':
+                                    f = open('prikol/dima.txt', 'r')
+                                    last_date = f.readline().rstrip()
+                                    record = f.readline().rstrip().split(" ")[1]
+                                    mes = 'рекорд дней без димы в вузе: ' + record + '..😤'
+                                    f.close()
+                            else:
+                                mes = 'клоун'
+
+                        for slov in queue.slovechki:
+                            if slov in text:
+                                mes = queue.slovechki[slov]
+
+                        if '!тейлор' in text:
+                            dir_name = 'taylor/'
+                            taylor_list = os.listdir(dir_name)
+                            upload = vk_api.VkUpload(self.vk)
+                            photo = upload.photo_messages(
+                                dir_name + taylor_list[random.randint(0, len(taylor_list) - 1)])
+                            owner_id = photo[0]['owner_id']
+                            photo_id = photo[0]['id']
+                            access_key = photo[0]['access_key']
+                            attachment = f'photo{owner_id}_{photo_id}_{access_key}'
+                            self.vk.messages.send(
+                                key=('0578ac1069eec95d993b7d9b479e82d61829fa26'),
+                                server=('https://lp.vk.com/wh200850856'),
+                                ts=('1'),
+                                random_id=get_random_id(),
+                                message="",
+                                attachment=attachment,
+                                chat_id=event.chat_id
+                            )
+                            continue
+
+                        if '!!очиститьвсё' in text:
+                            mes = queue.remove_all()
+
+                        if '!!смотретьвсё' in text:
+                            mes = queue.show_all()
+
+                        if '!команды' in text:
+                            f = open('text_files/commands.txt', 'r')
+                            mes = ''.join(f.readlines())
+                            f.close()
+
+                        self.vk.messages.send(
+                            key=('0578ac1069eec95d993b7d9b479e82d61829fa26'),
+                            server=('https://lp.vk.com/wh200850856'),
+                            ts=('1'),
+                            random_id=get_random_id(),
+                            message=mes,
+                            chat_id=event.chat_id
+                        )
+
+                elif event.from_user:
+                    pass
